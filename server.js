422ac43ee6c56c4f911ffdff4340a6b25b2c68c3
heini <melme900@gmail.com>
2021-07-19 10:18:09 +0900

    dd

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('hello webpack')
+})
+
+app.listen(80,()=>{
+    console.log('server start')
+})
